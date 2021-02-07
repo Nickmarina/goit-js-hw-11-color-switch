e7f6e339a3f16fd69ec905241bb5b4d5c8da518b
Marina Permiakova <marinapermyakova13@gmail.com>
2021-02-07 17:44:52 +0200

    add src directory

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import './styles.css';
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[ data-action="stop"]'),
+  body: document.querySelector('body'),
+};
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', startChangingColors);
+refs.stopBtn.addEventListener('click', stopChangingColors);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function startChangingColors() {
+  intervalId = setInterval(() => {
+    const randomColor = randomIntegerFromInterval(0, colors.length - 1);
+    refs.body.style.backgroundColor = colors[randomColor];
+    console.log(colors[randomColor]);
+    refs.startBtn.disabled = 'true';
+  }, 1000);
+}
+
+function stopChangingColors() {
+  refs.startBtn.removeEventListener;
+  clearInterval(intervalId);
+  // refs.startBtn.disabled = 'false';
+  refs.startBtn.removeAttribute('disabled');
+}
